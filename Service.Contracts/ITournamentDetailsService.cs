b0625f7096e1f050670884970e28b560e0d3ce1a
E-Rx <elise.rochaix@gmail.com>
2025-07-02 09:43:46 +0200

    Iservice Manage & SErviceManager

---
+++ Service.Contracts/ITournamentDetailsService.cs
@@ -0,0 +1,13 @@
+﻿using Tournament.Core.Dtos;
+
+namespace Service.Contracts
+{
+    public interface ITournamentDetailsService
+    {
+        Task<TournamentDetailsDto> GetTournamentDetailsAsync(int tournamentId);
+        Task<IEnumerable<TournamentDetailsDto>> GetAllTournamentsAsync();
+        Task<bool> CreateTournamentAsync(TournamentDetailsDto tournamentDetails);
+        Task<bool> UpdateTournamentAsync(TournamentDetailsDto tournamentDetails);
+        Task<bool> DeleteTournamentAsync(int tournamentId);
+    }
+}
---
+++ Tournament.Services/Services/ServiceManager.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Domain.Contracts.Repositories;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament.Services.Services
+{
+    public class ServiceManager : IServiceManager
+    {
+        private readonly Lazy<ITournamentDetailsService> tournamentDetailsService;
+        private readonly Lazy<IGameService> gameService;
+
+        public ITournamentDetailsService TournamentService => tournamentDetailsService.Value;
+        public IGameService GameService => gameService.Value;
+
+       
+        public ServiceManager(IUnitOfWork unitOfWork, IMapper mapper)
+
+        {
+            ArgumentNullException.ThrowIfNull(nameof(unitOfWork));
+
+            tournamentDetailsService = new Lazy<ITournamentDetailsService>(() => new TournamentDetailsService(unitOfWork, mapper));
+            gameService = new Lazy<IGameService>(() => new GameService(unitOfWork, mapper));
+        }
+
+       
+    }
+}
